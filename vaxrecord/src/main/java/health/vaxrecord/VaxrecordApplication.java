95f1d7c0ef05720a3a8a647121632d7598e4e8c1
ocho <ochoo11@yahoo.com>
2022-08-06 09:57:27 -0400

    created vaxrecord

---
+++ vaxrecord/src/main/java/health/vaxrecord/VaxrecordApplication.java
@@ -0,0 +1,13 @@
+package health.vaxrecord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaxrecordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaxrecordApplication.class, args);
+	}
+
+}
---
+++ vaxrecord/src/test/java/health/vaxrecord/VaxrecordApplicationTests.java
@@ -0,0 +1,13 @@
+package health.vaxrecord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaxrecordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
